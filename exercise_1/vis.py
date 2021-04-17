6c1307afe2a33b3ddb25153ee49cbcfb58cf09f6
Markus Fischer <markus.fischer@uni-jena.de>
2021-04-17 15:01:01 +0200

    ready?

--- exercise_1/vis.py
+++ exercise_1/vis.py
@@ -14,12 +14,11 @@ for benchmark in benchmarks:
         worker_string = "S0:1GB:" + str(i)
         benchmark_results = subprocess.run(["likwid-bench", "-t", benchmark, "-w", worker_string],
                                 capture_output=True).stdout
-        print(benchmark_results)
-        splitted_string = benchmark_results.split('\\n')
+        splitted_string = str(benchmark_results).split('\\n')
         flops = 0
         for string in splitted_string:
             if string.startswith("MFlops/s:"):
-                flops = float(string.split()[-1])
+                flops = float(string.split("\\t")[-1])
                 break
         print((i, flops))
         data.append((i, flops)) 
@@ -28,4 +27,26 @@ for benchmark in benchmarks:
     plt.scatter(*zip(*data))
     plt.ylabel("FLOPS")
     plt.xlabel("No. of cores")
-    plt.savefig(benchmark + ".png")
+    plt.savefig(benchmark + "_cores_" + ".png")
+    
+    data = []
+    for i in range(1, 33):
+        print(f"Threads: {i}")
+        worker_string = "S0:1GB:" + str(i)
+        benchmark_results = subprocess.run(["likwid-pin", "-c", "S0:0", "likwid-bench", "-t", benchmark, "-w", worker_string],
+                                capture_output=True).stdout
+        splitted_string = str(benchmark_results).split('\\n')
+        flops = 0
+        for string in splitted_string:
+            if string.startswith("MFlops/s:"):
+                flops = float(string.split("\\t")[-1])
+                break
+        print((i, flops))
+        data.append((i, flops)) 
+    plt.figure()
+    plt.title(benchmark)
+    plt.scatter(*zip(*data))
+    plt.ylabel("FLOPS")
+    plt.xlabel("No. of threads")
+    plt.savefig(benchmark + "_threads_" + ".png")
+
