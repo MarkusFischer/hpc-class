5f577b13d44c5a2b9cb389e121846d844d9a4b00
Markus Fischer <markus.fischer@uni-jena.de>
2021-05-09 16:33:06 +0200

    task 4.3

--- exercise_4/src/driver.cpp
+++ exercise_4/src/driver.cpp
@@ -1,5 +1,6 @@
 #include "util.hpp"
 #include "gemm_ref.hpp"
+#include "gemm_compiler_32_32_32_32_32_32.hpp"
 
 #include <vector>
 #include <iostream>
@@ -40,4 +41,65 @@ int main()
         delete[] B;
         delete[] C;
     }
+    
+    std::cout << "###########################" << std::endl << std::endl;
+    
+    float* A = random_matrix(32, 32, 32);
+    float* B = random_matrix(32, 32, 32);
+    float* C_ref = zero_matrix(32, 32, 32);
+    float* C_mnk = zero_matrix(32, 32, 32);
+    float* C_nkm = zero_matrix(32, 32, 32);
+      
+    std::cout << "Comparing results... " << std::endl;
+    gemm_ref(A, B, C_ref, 32, 32, 32, 32, 32, 32);
+    gemm_compiler_32_32_32_32_32_32_mnk(A, B, C_mnk);
+    gemm_compiler_32_32_32_32_32_32_nkm(A, B, C_nkm);
+    
+    if (compare_matrices(C_ref, C_mnk, 32, 32, 32, 32) && compare_matrices(C_ref, C_nkm, 32, 32, 32, 32))
+    {
+        std::cout << "Test passed!" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Test failed!" << std::endl;
+    }
+    
+    std::cout << "Measuring performance... " << std::endl;
+    int flop_gemm = 32 * 32 * (2 * 32 - 1);
+    std::cout << "Necessary floating point operations per gemm: " << flop_gemm << std::endl;
+    
+    std::cout << "gemm_compiler_32_32_32_32_32_32_mnk:" << std::endl;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < iterations; ++i) 
+    {
+        gemm_compiler_32_32_32_32_32_32_mnk(A, B, C_mnk);
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> needed_time = end_time - start_time;
+        
+    std::cout << "Needed time for " << iterations << " multiplications: " << needed_time.count() << std::endl;
+    std::cout << "Average time: " << needed_time.count() / iterations << std::endl;
+    std::cout << "Sustained GFLOPS: " << flop_gemm * iterations / needed_time.count() << std::endl;
+    
+    
+    std::cout << "gemm_compiler_32_32_32_32_32_32_nkm:" << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < iterations; ++i) 
+    {
+        gemm_compiler_32_32_32_32_32_32_nkm(A, B, C_mnk);
+    }
+    end_time = std::chrono::high_resolution_clock::now();
+    needed_time = end_time - start_time;
+        
+    std::cout << "Needed time for " << iterations << " multiplications: " << needed_time.count() << std::endl;
+    std::cout << "Average time: " << needed_time.count() / iterations << std::endl;
+    std::cout << "Sustained GFLOPS: " << flop_gemm * iterations / needed_time.count() << std::endl;
+    
+    
+    delete[] A;
+    delete[] B;
+    delete[] C_ref;
+    delete[] C_mnk;
+    delete[] C_nkm;
+
 }
--- exercise_4/src/util.cpp
+++ exercise_4/src/util.cpp
@@ -28,7 +28,9 @@ float* random_matrix(unsigned int i_m,
                      unsigned int i_ld)
 {
     if (i_ld < i_m)
+    {
         return nullptr;
+    }
     
     std::random_device rd;
     
@@ -42,6 +44,28 @@ float* random_matrix(unsigned int i_m,
         for (size_t n = 0; n < i_n; ++n)
         {
             matrix[(n * i_ld) + m] = dist(e2);
+        }
+    }
+    return matrix;
+}
+
+
+float* zero_matrix(unsigned int i_m, 
+                     unsigned int i_n, 
+                     unsigned int i_ld)
+{
+    if (i_ld < i_m)
+    {
+        return nullptr;
+    }
+    
+    float* matrix = new float[i_n * i_ld];
+    
+    for (size_t m = 0; m < i_m; ++m)
+    {
+        for (size_t n = 0; n < i_n; ++n)
+        {
+            matrix[(n * i_ld) + m] = 0;
         }
     }
     return matrix;
