f6f2d345ee8261fb125427282b305a4997d1f5e9
Markus Fischer <markus.fischer@uni-jena.de>
2021-05-24 17:32:41 +0200

    added driver

---
+++ exercise_5/task4/driver.cpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+#include <iostream>
+
+
+extern "C" 
+{
+void gemm_asm_gp( unit32_t const * i_a,
+                  uint32_t const * i_b,
+                  uint32_t       * io_c );
+
+}
+
+int main()
+{
+
+    uint32_t a[8] = {1, 2, 3, 5, 7, 11, 13, 17};
+    uint32_t b[4] = {1, 2, 3, 4};
+    uint32_t c[8] = {0};
+    
+    //NOTE more unit tests would be better but ... better than nothing
+    uint32_t c_res[8] = {15, 24, 29, 39, 31, 50, 61, 83};
+    
+    gemm_asm_gp(a, b, c);
+    
+    std::cout << "Comparing results... " << std::endl;
+    for (int i = 0; i < 8; ++i)
+    {
+        if (c[i] != c_res[i])
+        {
+            std::cout << "Found mismatch at index " << i << " c:" << c[i] << " != c_res:" << c_res[i] << std::endl; 
+        }
+    }
+    return 0;
+}
