2124b169333047c4974b1b637b1d5425d1926680
Markus Fischer <markus.fischer@uni-jena.de>
2021-05-24 16:27:44 +0200

    added simple test

--- exercise_5/task3/driver.cpp
+++ exercise_5/task3/driver.cpp
@@ -8,15 +8,49 @@ extern "C" {
                uint64_t       * o_b );
 }
 
+bool check_and_reset(uint32_t const * i_a, uint64_t* io_b)
+{
+    bool same = true;
+    for (int i = 0; i < 7; ++i)
+    {
+        if (i_a[i] != i_b[i])
+        {
+            same = false;
+        }
+        i_b[i] = 0;
+    }
+    return same;
+}
+
 int main() {
   uint32_t l_a[7] = { 1, 21, 43, 78, 89, 91, 93 };
   uint64_t l_b[7] = { 0 };
 
   copy_asm( l_a,
             l_b );
+  
+  std::cout << "copy_asm ";
+  if (check_and_reset(l_a, l_b))
+  {
+      std::cout << "succeed" << std::endl;
+  }
+  else
+  {
+      std::cout << "failed" << std::endl;
+  }
 
-  // copy_c( l_a,
-  //         l_b );
+   copy_c( l_a,
+           l_b );
 
+  std::cout << "copy_c ";
+  if (check_and_reset(l_a, l_b))
+  {
+      std::cout << "succeed" << std::endl;
+  }
+  else
+  {
+      std::cout << "failed" << std::endl;
+  }
+  
   return EXIT_SUCCESS;
 }
