e0c35fb8d76943a8fe5572c9ac045a077a9530be
Markus Fischer <markus.fischer@uni-jena.de>
2021-05-29 16:35:54 +0200

    ,

--- exercise_6/gemm_asm_asimd_16_4_4.s
+++ exercise_6/gemm_asm_asimd_16_4_4.s
@@ -28,19 +28,19 @@ gemm_asm_asimd_16_4_4:
         // your matrix-kernel goes here!
         
         //loading matrix C
-        ld1 {v0.4s, v1.4s, v2.4s, v3.4s} [x2]
+        ld1 {v0.4s, v1.4s, v2.4s, v3.4s}, [x2]
         add x2, x2, #16*4
-        ld1 {v4.4s, v5.4s, v6.4s, v7.4s} [x2]
+        ld1 {v4.4s, v5.4s, v6.4s, v7.4s}, [x2]
         add x2, x2, #16*4
-        ld1 {v8.4s, v9.4s, v10.4s, v11.4s} [x2]
+        ld1 {v8.4s, v9.4s, v10.4s, v11.4s}, [x2]
         add x2, x2, #16*4
-        ld1 {v12.4s, v13.4s, v14.4s, v15.4s} [x2]
+        ld1 {v12.4s, v13.4s, v14.4s, v15.4s}, [x2]
         
         //loading matrix B
-        ld1 {v16.4s, v17.4s, v18.4s, v19.4s} [x1]
+        ld1 {v16.4s, v17.4s, v18.4s, v19.4s}, [x1]
         
         //loading first row of A
-        ld1 {v20.4s, v21.4s, v22.4s, v23.4s} [x0]
+        ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
         
         //multiply add
         fmla v0.4s, v20.4s, v16.s[0]
@@ -65,7 +65,7 @@ gemm_asm_asimd_16_4_4:
         
         
         add x0, x0, #16*4
-        ld1 {v20.4s, v21.4s, v22.4s, v23.4s} [x0]
+        ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
         
         fmla v0.4s, v20.4s, v16.s[1]
         fmla v1.4s, v21.4s, v16.s[1]
@@ -89,7 +89,7 @@ gemm_asm_asimd_16_4_4:
         
         
         add x0, x0, #16*4
-        ld1 {v20.4s, v21.4s, v22.4s, v23.4s} [x0]
+        ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
         
         fmla v0.4s, v20.4s, v16.s[2]
         fmla v1.4s, v21.4s, v16.s[2]
@@ -113,7 +113,7 @@ gemm_asm_asimd_16_4_4:
         
         
         add x0, x0, #16*4
-        ld1 {v20.4s, v21.4s, v22.4s, v23.4s} [x0]
+        ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
         
         fmla v0.4s, v20.4s, v16.s[3]
         fmla v1.4s, v21.4s, v16.s[3]
@@ -137,13 +137,13 @@ gemm_asm_asimd_16_4_4:
         
         //store matrix C
         sub x2, x2, #3*16*4
-        st1 {v0.4s, v1.4s, v2.4s, v3.4s} [x2]
+        st1 {v0.4s, v1.4s, v2.4s, v3.4s}, [x2]
         add x2, x2, #16*4
-        st1 {v4.4s, v5.4s, v6.4s, v7.4s} [x2]
+        st1 {v4.4s, v5.4s, v6.4s, v7.4s}, [x2]
         add x2, x2, #16*4
-        st1 {v8.4s, v9.4s, v10.4s, v11.4s} [x2]
+        st1 {v8.4s, v9.4s, v10.4s, v11.4s}, [x2]
         add x2, x2, #16*4
-        st1 {v12.4s, v13.4s, v14.4s, v15.4s} [x2]
+        st1 {v12.4s, v13.4s, v14.4s, v15.4s}, [x2]
         
 
         // restore
