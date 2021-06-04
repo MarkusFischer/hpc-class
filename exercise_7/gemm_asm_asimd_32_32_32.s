14e63af7d4d1b20e8b444197b49c44f275e71da9
Markus Fischer <markus.fischer@uni-jena.de>
2021-06-04 17:12:13 +0200

    first 32 matrix implementation

--- exercise_7/gemm_asm_asimd_32_32_32.s
+++ exercise_7/gemm_asm_asimd_32_32_32.s
@@ -11,23 +11,39 @@
          * @param x2 pointer to C.
          */ 
 gemm_asm_asimd_32_32_32:
-        
+
+        mov x5, #8
+loop_n:
+
+        mov x4, #2
+loop_m:        
         //loading matrix C
         ld1 {v0.4s, v1.4s, v2.4s, v3.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         ld1 {v4.4s, v5.4s, v6.4s, v7.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         ld1 {v24.4s, v25.4s, v26.4s, v27.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         ld1 {v28.4s, v29.4s, v30.4s, v31.4s}, [x2]
-        sub x2, x2, #3*16*4
+        sub x2, x2, #3*2*16*4
         
-        //loading matrix B
-        ld1 {v16.4s, v17.4s, v18.4s, v19.4s}, [x1]
+        //inner k-loop handles 4x4 block of B from top to down
+        // since k = 32; have to iterate 32/4 = 8 times
+        mov x3, #8
+loop_k:
+        //loading block of matrix B
+        ldr v16, [x1], #16*8
+        ldr v17, [x1], #16*8
+        ldr v18, [x1], #16*8
+        ldr v19, [x1], #16*8
+
+        //move ptr 4 down
+        sub x1, x1, #4*16*8-16
+
         
         //loading first row of A
         ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
-        add x0, x0, #16*4
+        add x0, x0, #2*16*4
 
         //multiply add
         fmla v0.4s, v20.4s, v16.s[0]
@@ -53,7 +69,7 @@ gemm_asm_asimd_32_32_32:
         
         
         ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
-        add x0, x0, #16*4
+        add x0, x0, #2*16*4
 
         fmla v0.4s, v20.4s, v16.s[1]
         fmla v1.4s, v21.4s, v16.s[1]
@@ -77,7 +93,7 @@ gemm_asm_asimd_32_32_32:
         
         
         ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
-        add x0, x0, #16*4
+        add x0, x0, #2*16*4
 
         fmla v0.4s, v20.4s, v16.s[2]
         fmla v1.4s, v21.4s, v16.s[2]
@@ -100,8 +116,8 @@ gemm_asm_asimd_32_32_32:
         fmla v31.4s, v23.4s, v19.s[2]
         
         
-        
         ld1 {v20.4s, v21.4s, v22.4s, v23.4s}, [x0]
+        add x0, x0, #2*16*4
         
         fmla v0.4s, v20.4s, v16.s[3]
         fmla v1.4s, v21.4s, v16.s[3]
@@ -123,16 +139,40 @@ gemm_asm_asimd_32_32_32:
         fmla v30.4s, v22.4s, v19.s[3]
         fmla v31.4s, v23.4s, v19.s[3]
         
+        sub x3, x3, #1
+        cbnz x3, loop_k
+
+        //inner loop_k finished
+        //move A ptr down
+        sub x0, x0, #2*16*4*4*8-16*4
+        //reset B ptr
+        sub x1, x1, #3*32*4+28*4
+
         //store matrix C
         
         st1 {v0.4s, v1.4s, v2.4s, v3.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         st1 {v4.4s, v5.4s, v6.4s, v7.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         st1 {v24.4s, v25.4s, v26.4s, v27.4s}, [x2]
-        add x2, x2, #16*4
+        add x2, x2, #2*16*4
         st1 {v28.4s, v29.4s, v30.4s, v31.4s}, [x2]
-        
+        sub x2, x2, #3*2*16*4-16*4
+
+        sub x4, x4, #1
+        cbnz x4, loop_m
+
+        //set C ptr to 5th column, first element
+        add x2, x2, #3*32*4+4*16
+
+        //set A ptr to 1th element
+        sub x0, x0, #32*4*31+28*4
+
+        //set B ptr to 5th column, first element
+        add x1, x1, #3*32*4+4*16
+
+        sub x5, x5, #1
+        cbnz x5, loop_n
 
         ret
         .size gemm_asm_asimd_32_32_32, (. - gemm_asm_asimd_32_32_32)
