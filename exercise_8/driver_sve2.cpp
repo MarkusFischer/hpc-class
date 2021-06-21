aa939e2de4b057f423c645d5d66915d202069dd5
Markus Fischer <markus.fischer@uni-jena.de>
2021-06-21 13:38:16 +0200

    wrong signature

--- exercise_8/driver_sve2.cpp
+++ exercise_8/driver_sve2.cpp
@@ -3,17 +3,17 @@
 
 extern "C" 
 {
-    void fmlalb(float* const a, float* const b, float* c);
-    void fmlalt(float* const a, float* const b, float* c); 
-    void udot(float* const a, float* const b, float* c);
+    void fmlalb_example(float* const a, float* const b, float* c);
+    void fmlalt_example(float* const a, float* const b, float* c); 
+    void udot_example(float* const a, float* const b, float* c);
 }
 
 
 int main()
 {
-    uint32_t dataIn1[128] = {0};
-    uint32_t dataIn2[128] = {0};
-    uint32_t dataOut[128] = {0};
+    float dataIn1[128] = {0};
+    float dataIn2[128] = {0};
+    float dataOut[128] = {0};
 
     for (unsigned int i = 0; i < 128; ++i)
     {
@@ -23,7 +23,7 @@ int main()
     return 0;
 
     std::cout << "fmlalb kernel" << std::endl;
-    fmlalb(dataIn1, dataIn2, dataOut);
+    fmlalb_example(dataIn1, dataIn2, dataOut);
 
     for (unsigned int i = 0; i < 128; ++i)
     {
@@ -32,7 +32,7 @@ int main()
 
 
     std::cout << std::endl << std::endl << "fmlalt kernel" << std::endl;
-    fmlalt(dataIn1, dataIn2, dataOut);
+    fmlalt_example(dataIn1, dataIn2, dataOut);
 
     for (unsigned int i = 0; i < 128; ++i)
     {
@@ -40,7 +40,7 @@ int main()
     }
 
     std::cout << std::endl << std::endl << "udot kernel" << std::endl;
-    udot(dataIn1, dataIn2, dataOut);
+    udot_example(dataIn1, dataIn2, dataOut);
 
     for (unsigned int i = 0; i < 128; ++i)
     {
