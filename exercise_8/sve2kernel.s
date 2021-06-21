9632815cc1c5723ef2e219af83977c666f73b8d7
Markus Fischer <markus.fischer@uni-jena.de>
2021-06-21 13:43:15 +0200

    added output

--- exercise_8/sve2kernel.s
+++ exercise_8/sve2kernel.s
@@ -1,19 +1,5 @@
     .text
     .align 4
-    .type sve_example_0, %function
-    .global sve_example_0
-sve_example_0:
-    ldr z0, [x0]
-    str z0, [x1]
-    ret
-
-    .type sve_example_2, %function
-    .global sve_example_2
-sve_example_2:
-    ptrue p0.s, VL8
-    ld1w {z0.s}, p0/z, [x0]
-    str z0, [x1]
-    ret
 
     .type fmlalb_example, %function
     .global fmlalb_example
